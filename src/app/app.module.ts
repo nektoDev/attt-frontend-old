969f8f745e9907de1f38287793325a7ce2ecfac7
nektodev <ts.slawa@gmail.com>
2017-04-04 08:24:41 +0300

    Add test table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {Ng2TableModule} from 'ng2-table/ng2-table';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    Ng2TableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
