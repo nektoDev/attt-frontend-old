9ab5067aee012cd181326be15e350c984cd18fa9
nektodev <ts.slawa@gmail.com>
2017-04-11 23:50:12 +0300

    Add search component

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SearchRoutingModule } from './search-routing.module';
+import { SearchComponent } from './search.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SearchRoutingModule
   ],
-  declarations: []
+  declarations: [SearchComponent]
 })
 export class SearchModule { }
