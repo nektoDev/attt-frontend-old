75f5f8d6c1423b87a12108219c4c7f1e26ebaa5f
nektodev <ts.slawa@gmail.com>
2017-04-11 23:54:05 +0300

    Add search link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ export const routes: Routes = [
   { path: '', redirectTo: "torrents", pathMatch: 'full'},
   { path: 'settings', loadChildren: 'app/settings/settings.module#SettingsModule' },
   { path: 'torrents', loadChildren: 'app/torrents/torrents.module#TorrentsModule' },
-  { path: 'search', loadChildren: 'app/torrents/search.module#SearchModule' },
+  { path: 'search', loadChildren: 'app/search/search.module#SearchModule' },
 ];
 
 @NgModule({
