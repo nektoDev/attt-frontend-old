96132e268b1f0ed2c0e95fc1ca07b9451d499ff1
nektodev <ts.slawa@gmail.com>
2017-04-12 08:17:12 +0300

    Add simple table

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -11,6 +11,18 @@ export class SearchComponent implements OnInit {
   public query: string;
   public founded: FoundedTorrent[];
 
+  public columns: Array<any> = [
+    {title: 'Name', name: 'name', sort: 'asc'},
+    {title: 'URL', name: 'url', type: "url", sort: 'asc'},
+    {title: 'Category', name: 'category', sort: 'asc'},
+    {title: 'Seeders', name: 'seeders', sort: 'asc'},
+    {title: 'Size', name: 'size', sort: 'asc', pipes: ''},
+  ];
+
+  public config: any = {
+    sorting: {name: 'size', sort: 'desc'},
+  };
+
   constructor(private searchService: SearchService) { }
 
   ngOnInit() {
@@ -19,5 +31,63 @@ export class SearchComponent implements OnInit {
 
   onSubmit() {
     this.searchService.search(this.query).subscribe( r => this.founded = r);
+  }
+
+  public onChangeTable(config: any): any {
+    if (config.filtering) {
+      Object.assign(this.config.filtering, config.filtering);
+    }
+
+    if (config.sorting) {
+      Object.assign(this.config.sorting, config.sorting);
+    }
+
+    this.founded = this.changeSort(this.founded, this.config);
+  }
+
+  public changeSort(data: any, config: any): any {
+    if (!config.sorting) {
+      return data;
+    }
+
+    let columnName: string = this.config.sorting.name;
+    let sort: string = this.config.sorting.sort;
+
+    if (!columnName) {
+      return data;
+    }
+
+    // simple sorting
+    return data.sort((previous: any, current: any) => {
+      if (previous[columnName] > current[columnName]) {
+        return sort === 'desc' ? -1 : 1;
+      } else if (previous[columnName] < current[columnName]) {
+        return sort === 'asc' ? -1 : 1;
+      }
+      return 0;
+    });
+  }
+
+  public getColumnSort(columnName: string) {
+
+    if (this.config.sorting) {
+      if (this.config.sorting.name === columnName) {
+        return this.config.sorting.sort;
+      }
+    }
+    return null;
+  }
+
+  public onHeaderClick(column: any) {
+
+    if (this.config.sorting) {
+      if (this.config.sorting.name === column.name) {
+        this.config.sorting.sort = this.config.sorting.sort === 'asc' ? 'desc' : 'asc';
+      } else {
+        Object.assign(this.config.sorting, column)
+      }
+    }
+
+    this.onChangeTable(this.config);
   }
 }
